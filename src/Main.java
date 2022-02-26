9abc094e9280e8ffff2f9ad6dc25d8cc093ec80d
BESKAR <mityaaabl@gmail.com>
2022-02-26 18:43:52 +0300

    Bonus Calculation v.3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // параметров программы: начального счёта,
         // суммы пополнения и тп
         int customerAccount = 500;
-        int refillAccount = 1300;
+        int refillAccount = 1000;
 
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
@@ -15,7 +15,7 @@ public class Main {
             int totalSum = customerAccount + refillAccount;
             System.out.println(" бонусов нет, на счету " + totalSum + " рублей. ");
         } else {
-            if (refillAccount > 1000) {
+            if (refillAccount >= 1000) {
                 int totalSum = customerAccount + refillAccount;
                 int bBonus = refillAccount / 100;
                 int aBonus = totalSum + bBonus;
